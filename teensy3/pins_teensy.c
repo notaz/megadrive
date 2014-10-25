328c1b3394b711135fed11225cccef84220ef6be
notaz <notasas@gmail.com>
2014-10-26 01:14:39 +0300

    micros() implementation without irq disable

--- teensy3/pins_teensy.c
+++ teensy3/pins_teensy.c
@@ -690,7 +690,7 @@ volatile uint32_t systick_millis_count = 0;
 
 //uint32_t systick_current, systick_count, systick_istatus;  // testing only
 
-uint32_t micros(void)
+static uint32_t micros_mask(void)
 {
 	uint32_t count, current, istatus;
 
@@ -703,6 +703,24 @@ uint32_t micros(void)
 	 //systick_count = count;
 	 //systick_istatus = istatus & SCB_ICSR_PENDSTSET ? 1 : 0;
 	if ((istatus & SCB_ICSR_PENDSTSET) && current > 50) count++;
+	current = ((F_CPU / 1000) - 1) - current;
+	return count * 1000 + current / (F_CPU / 1000000);
+}
+
+uint32_t micros(void)
+{
+	uint32_t count, current, istatus;
+
+	if (nvic_execution_priority() <= (SCB_SHPR3 >> 24))
+		return micros_mask();
+
+	do {
+		current = SYST_CVR;
+		count = systick_millis_count;
+		istatus = SCB_ICSR;	// bit 26 indicates if systick exception pending
+	}
+	while (istatus & SCB_ICSR_PENDSTSET);
+
 	current = ((F_CPU / 1000) - 1) - current;
 	return count * 1000 + current / (F_CPU / 1000000);
 }
